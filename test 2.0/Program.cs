925ec255c78ce60fe828bd0449f07d5ca6d56010
cisco <cisco@DESKTOP-D4D1G54>
2022-09-21 13:05:28 +0200

    Add project files.

---
+++ test 2.0/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test_2._0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Adjon meg 2 darab számot!");
+            int szam1 = Convert.ToInt32(Console.ReadLine());
+            int szam2 = Convert.ToInt32(Console.ReadLine());
+            
+    }
+}
